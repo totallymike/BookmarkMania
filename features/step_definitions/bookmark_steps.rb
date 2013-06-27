f5414357319d8a330b99339b5cab77dba922737f
Michael Westbom <totallymike@gmail.com>
2013-06-27 19:14:57 -0400

    Added stubs for remaining steps.

--- features/step_definitions/bookmark_steps.rb
+++ features/step_definitions/bookmark_steps.rb
@@ -1,3 +1,19 @@
-When /the home page/ do
+Given /the home page/ do
   visit(root_path)
+end
+
+When /^I fill in "(.*?) with "(.*?)"$/ do |input, value|
+  pending
+end
+
+When /^I press "(.*?)"$/ do |button|
+  pending
+end
+
+Then /the bookmark page$/ do
+  pending
+end
+
+Then /^I should see "(.*?)$/ do |content|
+  pending
 end
