0b5203b54a5e2140e0d28e43860eadc80b20d7a8
Michael Westbom <totallymike@gmail.com>
2013-06-27 18:24:14 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookmarkMania::Application.config.secret_key_base = '5586304a6118187d6073f72f381f1c3427a11f28f2e86e6f52e74f100a350d5b7beef7d3c8da9be03e1f2991d23c98c88d08bb64d0715130f0a737a8e9b9391b'
