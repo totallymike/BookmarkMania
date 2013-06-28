07873d25da4eae5b4ec075187c8ee880f5bb364e
Michael Westbom <totallymike@gmail.com>
2013-06-28 12:54:55 -0400

    Eliminated unnecessary column.

--- db/migrate/20130628010259_create_bookmarks_table.rb
+++ db/migrate/20130628010259_create_bookmarks_table.rb
@@ -2,7 +2,6 @@ class CreateBookmarksTable < ActiveRecord::Migration
   def change
     create_table :bookmarks do |t|
       t.string :url
-      t.string :tags # This won't be a string forever.  For now it's fine.
 
       t.timestamps
     end
