1b3c3b9dba5efcc6130e0bde21d4a71027d2689c
Michael Westbom <totallymike@gmail.com>
2013-06-28 13:39:37 -0400

    Tags now exist as entities of their own.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,10 @@
 class Bookmark < ActiveRecord::Base
   has_and_belongs_to_many :tags
 
+  def tags_list
+    self.tags.join(' ')
+  end
+
   def tags_list=(tags_string)
     tag_names = tags_string.split(', ')
     self.tags = tag_names.map{|tag| Tag.find_or_create_by(name: tag)}
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :bookmarks
+
+  def to_s
+    self.name
+  end
 end
