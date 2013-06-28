7afa595595850edad3f9d3b0068abf569bd1c002
Michael Westbom <totallymike@gmail.com>
2013-06-28 13:06:02 -0400

    Cleaned up spec a bit.

--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -1,23 +1,28 @@
 require 'spec_helper'
 
 describe BookmarksController do
-  it 'GET #show' do
-    bookmark = create(:bookmark)
-    get :show, id: bookmark
-    expect(assigns(:bookmark)).to eq bookmark
+  describe 'GET #show' do
+    it 'presents a bookmark upon success' do
+      bookmark = create(:bookmark)
+      get :show, id: bookmark
+      expect(assigns(:bookmark)).to eq bookmark
+    end
   end
 
   describe 'POST #create' do
     let(:post_create) {
       post :create,
-      bookmark: attributes_for(:bookmark, tags_list: 'Tag1, Tag2')
+        bookmark: attributes_for(:bookmark, tags_list: 'Tag1, Tag2')
     }
+
     it 'accepts a url and some tags and creates a bookmark' do
       expect{post_create}.to change(Bookmark, :count).by 1
     end
+
     it 'instantiates tags from the provided list' do
       expect{post_create}.to change(Tag, :count).by 2
     end
+
     it 'Does not recreate tags that already exist' do
       create(:tag, name: 'Tag1')
       create(:tag, name: 'Tag2')
