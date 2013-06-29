3b7d58710a91611c1c295fbf6a421b2491443852
Michael Westbom <totallymike@gmail.com>
2013-06-29 10:45:41 -0400

    Fixed generators.
    Now the rails generator config mirrors what I'm actually using.
    Save me some time doing it myself.

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,14 @@ module BookmarkMania
   class Application < Rails::Application
     config.generators do |g|
       g.template_engine :haml
+      g.test_framework :rspec,
+        fixture: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
