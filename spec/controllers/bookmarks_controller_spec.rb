35b00d9cc6df4b4babd57f5dc6b1326f8986f123
Michael Westbom <totallymike@gmail.com>
2013-06-29 14:03:55 -0400

    Added coverage for no-tags situation.
    Tags are optional; the app must allow their omission.

--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -10,6 +10,11 @@ describe BookmarksController do
   end
 
   describe 'POST #create' do
+    let(:post_no_tags) {
+      post :create,
+        bookmark: attributes_for(:bookmark)
+    }
+
     let(:post_create) {
       post :create,
         bookmark: attributes_for(:bookmark, tags_list: 'Tag1, Tag2')
@@ -17,6 +22,10 @@ describe BookmarksController do
 
     it 'accepts a url and some tags and creates a bookmark' do
       expect{post_create}.to change(Bookmark, :count).by 1
+    end
+
+    it 'accepts a url and no tags and creates a bookmark' do
+      expect{post_no_tags}.to change(Bookmark, :count).by 1
     end
 
     it 'instantiates tags from the provided list' do
