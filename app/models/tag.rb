95166fc48e65ac3cf24e0a981dc58f13685d6457
Michael Westbom <totallymike@gmail.com>
2013-06-29 14:55:06 -0400

    Tags must be uniquely named.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,8 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :bookmarks
 
+  validates :name, uniqueness: true
+
   def to_s
     self.name
   end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,4 +5,11 @@ describe Tag do
     tag = create(:tag)
     expect(tag.to_s).to eq tag.name
   end
+
+  describe 'Validations' do
+    it 'enforces uniqueness of tags' do
+      create(:tag, name: 'Tag1')
+      expect(build(:tag, name: 'Tag1')).to have(1).errors_on(:name)
+    end
+  end
 end
