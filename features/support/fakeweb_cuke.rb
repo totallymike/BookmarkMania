72ea43868ef658c53247c30af339fe9fa841626a
Michael Westbom <totallymike@gmail.com>
2013-06-30 15:17:59 -0400

    Add support in Cucumber for the FakeWeb behavior.
    This blocks all communication with real web services, and provides a stub
    for a fake page.  Isolation!

---
+++ features/support/fakeweb_cuke.rb
@@ -0,0 +1,13 @@
+require 'fakeweb'
+
+FakeWeb.allow_net_connect = false
+
+page_body = <<-end_of_page
+  <!DOCTYPE html>
+  <html>
+    <head><title>Example Page Number 1!</title></head>
+    <body>A web page.</body>
+  </html>
+end_of_page
+
+FakeWeb.register_uri(:get, 'http://example.com/page1', body: page_body)
