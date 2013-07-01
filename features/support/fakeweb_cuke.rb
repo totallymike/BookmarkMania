9e6fc121864281161275746e3066e5e54b7458e9
Michael Westbom <totallymike@gmail.com>
2013-07-01 18:02:27 -0400

    Changed FakeWeb uri to allow any method.

--- features/support/fakeweb_cuke.rb
+++ features/support/fakeweb_cuke.rb
@@ -10,4 +10,4 @@ page_body = <<-end_of_page
   </html>
 end_of_page
 
-FakeWeb.register_uri(:get, 'http://example.com/page1', body: page_body)
+FakeWeb.register_uri(:any, 'http://example.com/page1', body: page_body)
