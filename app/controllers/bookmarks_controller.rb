17cbd3ec822ef63b919726538388e70322e2caee
Michael Westbom <totallymike@gmail.com>
2013-07-02 18:28:54 -0400

    Added a builder for short URLs, updated view.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,7 @@
 class BookmarksController < ApplicationController
   def show
     @bookmark = Bookmark.find(params[:id])
+    @short_url = build_short_url(@bookmark)
   end
 
   def create
@@ -14,7 +15,16 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def short_url
+    @bookmark = Bookmark.find_by(shortened_url: params[:short_url])
+    redirect_to @bookmark.url, status: 301
+  end
+
   private
+  def build_short_url(bookmark)
+      request.raw_host_with_port <<
+      short_url_path(bookmark.shortened_url)
+  end
   def bookmark_params
     params.require(:bookmark).permit(:url, :tags_list)
   end
