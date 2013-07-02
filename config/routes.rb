975b59a8e97c4520c4d2d97edd9b3bc0cf0aad16
Michael Westbom <totallymike@gmail.com>
2013-07-02 18:03:56 -0400

    Add route for short URLs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 BookmarkMania::Application.routes.draw do
   resources :bookmarks
+  get '/:short_url',
+      to: 'bookmarks#short_url',
+      short_url: /\w{6}/,
+      as: :short_url
   root to: 'welcome#index'
 end
