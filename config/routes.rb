5e90602a786e83a07efea04c62b7b27b11323672
Michael Westbom <totallymike@gmail.com>
2013-07-03 14:00:49 -0400

    Removed dead actions from routes.
    Also deleted a link from the sites index view to SitesController#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 BookmarkMania::Application.routes.draw do
   resources :bookmarks, only: [:show, :short_url, :create]
-  resources :sites
-  resources :tags
+  resources :sites, only: [:show, :index]
+  resources :tags, only: [:show, :index]
   get '/:short_url',
       to: 'bookmarks#short_url',
       short_url: /\w{6}/,
