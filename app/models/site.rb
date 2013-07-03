11224160af5f82901f2fd5dfd2d6d50d58570927
Michael Westbom <totallymike@gmail.com>
2013-07-03 12:24:24 -0400

    Site now collects metadata for itself.

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,9 @@
 class Site < ActiveRecord::Base
+  before_create :assign_page_attributes
   has_many :bookmarks
+
+  private
+  def assign_page_attributes
+    PageIdentifier.new(self).assign_attributes!
+  end
 end
