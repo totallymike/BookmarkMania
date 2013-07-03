2bf4be2cdccd6148934310c96209d3ba364a66dc
Michael Westbom <totallymike@gmail.com>
2013-07-03 13:20:14 -0400

    Expect Tag.with_bookmarks to omit bookmarkless tags.

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe Tag do
+  describe '.with_bookmarks' do
+    it 'omits tags with no bookmarks attached' do
+      create(:bookmark, tags_list: 'Tag1')
+      tag2 = create(:tag, name: 'Tag2')
+
+      expect(Tag.with_bookmarks).to_not include(tag2)
+    end
+  end
+
   it 'renders the name as a string' do
     tag = create(:tag)
     expect(tag.to_s).to eq tag.name
