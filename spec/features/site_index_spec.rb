a7895315a289842e9c1dbabed5344234b0266617
Michael Westbom <totallymike@gmail.com>
2013-07-03 10:54:58 -0400

    Site index should display links as titles of sites.

---
+++ spec/features/site_index_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature 'The index of sites lists the titles of all sites' do
+  scenario 'A user views the list of sites' do
+    create(:bookmark)
+    create(:bookmark, url: 'http://reddit.com/r/ruby')
+    visit(sites_path)
+
+    expect(page).to have_link('Example!')
+    expect(page).to have_link('reddit: the front page of the internet')
+  end
+end
