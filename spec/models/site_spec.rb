10bdd5081c32a2d1dd4f702ab293936a097898dc
Michael Westbom <totallymike@gmail.com>
2013-07-03 10:58:54 -0400

    Site title should be fetched from root of domain.

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Site do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#title' do
+    it 'displays the title of the root page on a domain' do
+      root_bookmark = create(:bookmark, url: 'http://example.com')
+      bookmark = create(:bookmark, url: 'http://example.com/page1')
+
+      expect(root_bookmark.site.title).to eq 'Example!'
+      expect(bookmark.site.title).to eq root_bookmark.site.title
+    end
+  end
 end
