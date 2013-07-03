7a5daf5fcb6b73a0484776791ac8474fe648d000
Michael Westbom <totallymike@gmail.com>
2013-07-03 13:59:10 -0400

    Removed unnecessary TagController actions.
    Tags should not be directly manipulated.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
-  before_action :set_tag, only: [:show, :edit, :update, :destroy]
+  before_action :set_tag, only: [:show]
 
   # GET /tags
   # GET /tags.json
@@ -11,56 +11,6 @@ class TagsController < ApplicationController
   # GET /tags/1.json
   def show
   end
-
-  # GET /tags/new
-  def new
-    @tag = Tag.new
-  end
-
-  # GET /tags/1/edit
-  def edit
-  end
-
-  # POST /tags
-  # POST /tags.json
-  def create
-    @tag = Tag.new(tag_params)
-
-    respond_to do |format|
-      if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @tag }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /tags/1
-  # PATCH/PUT /tags/1.json
-  def update
-    respond_to do |format|
-      if @tag.update(tag_params)
-        format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /tags/1
-  # DELETE /tags/1.json
-  def destroy
-    @tag.destroy
-    respond_to do |format|
-      format.html { redirect_to tags_url }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_tag
