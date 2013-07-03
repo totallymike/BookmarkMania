b0248542abf3355e703dda79b7766317ea922e47
Michael Westbom <totallymike@gmail.com>
2013-07-03 17:42:08 -0400

    Teach User model about Site association.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :bookmarks
+  has_many :sites
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
