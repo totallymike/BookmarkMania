f9e30a76df0a5bf12849036a5dd4fd028f370d5d
Michael Westbom <totallymike@gmail.com>
2013-07-03 12:44:07 -0400

    Add tags to route; show bookmarks in tag view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 BookmarkMania::Application.routes.draw do
   resources :bookmarks, only: [:show, :short_url, :create]
   resources :sites
+  resources :tags
   get '/:short_url',
       to: 'bookmarks#short_url',
       short_url: /\w{6}/,
