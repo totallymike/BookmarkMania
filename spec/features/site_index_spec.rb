91c6765532f8182df848168dab479be231407d92
Michael Westbom <totallymike@gmail.com>
2013-07-03 10:55:52 -0400

    Reddit URL didn't match FakeWeb registered URI.

--- spec/features/site_index_spec.rb
+++ spec/features/site_index_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'The index of sites lists the titles of all sites' do
   scenario 'A user views the list of sites' do
     create(:bookmark)
-    create(:bookmark, url: 'http://reddit.com/r/ruby')
+    create(:bookmark, url: 'http://www.reddit.com/r/ruby')
     visit(sites_path)
 
     expect(page).to have_link('Example!')
