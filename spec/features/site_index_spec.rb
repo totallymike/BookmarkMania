adf5a26f09584fc19d5da18042513e9becd35774
Michael Westbom <totallymike@gmail.com>
2013-07-04 15:21:59 -0400

    Update spec to better drive website.
    Use the login functionality of the page, as well as the bookmark form.

--- spec/features/site_index_spec.rb
+++ spec/features/site_index_spec.rb
@@ -2,8 +2,21 @@ require 'spec_helper'
 
 feature 'The index of sites lists the titles of all sites' do
   scenario 'A user views the list of sites' do
-    create(:bookmark)
-    create(:bookmark, url: 'http://www.reddit.com/r/ruby')
+    user = create(:user)
+
+    visit(new_user_session_path)
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Sign in'
+
+    visit root_path
+    fill_in 'bookmark_url', with: 'http://example.com/page1'
+    click_button 'Save'
+
+    visit root_path
+    fill_in 'bookmark_url', with: 'http://www.reddit.com/r/ruby'
+    click_button 'Save'
+
     visit(sites_path)
 
     expect(page).to have_link('Example!')
