9bb0182a01764d40e51f70fabc27b7a0381de76a
Michael Westbom <totallymike@gmail.com>
2013-07-04 15:56:04 -0400

    Add scenario where a user doesn't use a tag.

--- spec/features/tag_index_spec.rb
+++ spec/features/tag_index_spec.rb
@@ -1,12 +1,46 @@
 require 'spec_helper'
 
-feature 'The index of sites lists the titles of all sites' do
+feature 'The index of tags lists the names of all tags' do
   scenario 'A user views the list of tags' do
-    create(:bookmark_with_tags)
+    user = create(:user)
+    visit root_path
+    click_link 'Log in'
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Sign in'
+
+    visit root_path
+    fill_in 'bookmark_url', with: 'http://example.com/page1'
+    fill_in 'bookmark_tags_list', with: 'Tag1, Tag2'
+    click_button 'Save'
+
     visit(tags_path)
 
     expect(page).to have_link('Tag1')
     expect(page).to have_link('Tag2')
+  end
+
+  scenario "The tags index does not display tags a user doesn't use" do
+    user = create(:user)
+    unattached_bookmark = create(:bookmark,
+      url: 'http://example.com/page2',
+      tags_list: 'Tag2')
+
+    visit root_path
+    click_link 'Log in'
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Sign in'
+
+    visit root_path
+    fill_in 'bookmark_url', with: 'http://example.com/page1'
+    fill_in 'bookmark_tags_list', with: 'Tag1, Tag2'
+    click_button 'Save'
+
+    visit tags_path
+
+    expect(page).to have_link('Tag1')
+    expect(page).to_not have_link('Tag2')
   end
 
   scenario "The tags index does not display tags with no bookmarks" do
