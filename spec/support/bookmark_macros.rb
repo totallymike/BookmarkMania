f7ac55db8d0830370f0dde72def395f62d307933
Michael Westbom <totallymike@gmail.com>
2013-07-05 09:56:17 -0400

    Let bookmark macro return the bookmark.

--- spec/support/bookmark_macros.rb
+++ spec/support/bookmark_macros.rb
@@ -4,5 +4,7 @@ module BookmarkMacros
     fill_in 'bookmark_url', with: url
     fill_in 'bookmark_tags_list', with: tags_list if tags_list
     click_button 'Save'
+
+    Bookmark.find_by(url: url)
   end
 end
