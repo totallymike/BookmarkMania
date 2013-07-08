baf27a296ac63bd1f65a90bd3d5e58c1faa1b0a1
Michael Westbom <totallymike@gmail.com>
2013-07-08 10:37:39 -0400

    Add search query for bookmarks.
    This query will be moved from the controller.  It's only here now to
    pass the test.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def new
-
+    search_string = "%#{params[:search]}%"
+    @bookmarks = Bookmark.where {
+      url.matches(search_string) | title.matches(search_string)
+    }
   end
 end
